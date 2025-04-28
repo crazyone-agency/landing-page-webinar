4f23150eb3b82c7cc54be07779db96a749b20423
Ivan Ravasi <ivan4.ravasi@gmail.com>
2025-04-28 16:16:25 +0200

    Show floating CTA only in the home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import {Switch, Route, useLocation} from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -12,15 +12,20 @@ import SubscribePage from "@/pages/SubscribePage";
 import NotFound from "@/pages/not-found";
 
 function Router() {
+  const [location] = useLocation();
+
   return (
-    <Switch>
-      <Route path="/" component={LandingPage} />
-      <Route path="/corso-sviluppo-personale" component={CoursePage} />
-      <Route path="/offerta-speciale" component={OtoPage} />
-      <Route path="/checkout" component={CheckoutPage} />
-      <Route path="/subscribe" component={SubscribePage} />
-      <Route component={NotFound} />
-    </Switch>
+    <>
+      <Switch>
+        <Route path="/" component={LandingPage} />
+        <Route path="/corso-sviluppo-personale" component={CoursePage} />
+        <Route path="/offerta-speciale" component={OtoPage} />
+        <Route path="/checkout" component={CheckoutPage} />
+        <Route path="/subscribe" component={SubscribePage} />
+        <Route component={NotFound} />
+      </Switch>
+      {location == '/' && <FloatingCta />}
+    </>
   );
 }
 
@@ -30,7 +35,6 @@ function App() {
       <TooltipProvider>
         <Toaster />
         <Router />
-        <FloatingCta />
       </TooltipProvider>
     </QueryClientProvider>
   );
